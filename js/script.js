5d9d646dc737f65720734e6f90f10c9c57b4c128
Jaka Nursobah <124447602+jnursobah@users.noreply.github.com>
2023-03-24 19:10:09 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,3 +34,25 @@ document.querySelectorAll('a').forEach(links =>{
     }
 
 });
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+}
+
+const serviceID = "service_6ztfwl7";
+const templateID = "template_egkh99f";
+
+emailjs
+.send(serviceID, templateID, params)
+.then((res) => {
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert(Your message has been sent);
+})
+.catch((err) => console.log(err));
